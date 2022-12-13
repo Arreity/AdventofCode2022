c65b821f5bcc708316fbe9213f296982c0fa8d51
Charlie Jung <charlie@Zaqs-MacBook-Pro.local>
2022-12-12 23:48:29 -0800

    cleaned up the code of notes

--- day 4/solution.py
+++ day 4/solution.py
@@ -9,10 +9,8 @@ for string in Lines:
 
     left = parts[0]
     right = parts[1]
-    #print(left)
 
     def get_range(chores):
-        # Split the string on the dash
         numbers = chores.split('-')
         numbers = [int(n) for n in numbers]
 
@@ -22,18 +20,14 @@ for string in Lines:
     
     left_range = get_range(left)
     right_range = get_range(right)
-    #print(left_range)
-    #print(right_range)
-
+    
+    #part1
     if left_range >= right_range or right_range >= left_range:
         total_full_overlap += 1
-    
+    #part2
     if left_range & right_range:
         total_general_overlap += 1
 
 print(f"The total tasks containing others is: {total_full_overlap}\nTotal tasks mixed is: {total_general_overlap} ")
 
-#break line and assigned left string to elf_L, elf_R
-#Compare L numbers to R, if any common numbers count, else ignore
-#assign that count to total
 
